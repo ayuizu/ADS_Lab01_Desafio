c629aacf585461f6cc83e0cc449d694448426b88
ayuizu <ayuizu0519@gmail.com>
2024-06-05 18:10:32 -0300

    v02

---
+++ Desafio_Etapa2/Loja.java
@@ -0,0 +1,109 @@
+
+/**
+ * Lohas de E0 a E19
+ */
+public class Loja
+{
+    //Atributos
+    private String nome;
+    private int quantidadeFuncionarios;
+    private double salarioBaseFuncionario;
+    private Endereco endereco;
+    private Data dataFundacao;
+    
+    //Métodos construtores
+    ////Vazio
+    public Loja(){
+        
+    }
+    ////Todos - Etapa1
+    public Loja(String nome,int quantidadeFuncionarios,double salarioBaseFuncionario){
+        this.nome=nome;
+        this.quantidadeFuncionarios=quantidadeFuncionarios;
+        this.salarioBaseFuncionario=salarioBaseFuncionario;
+    }
+    ////Só nome e qtd funcionarios - Etapa1
+    public Loja(String nome,int quantidadeFuncionarios){
+        this.nome=nome;
+        this.quantidadeFuncionarios=quantidadeFuncionarios;
+        this.salarioBaseFuncionario=-1;   
+    }
+    ////Todos - Etapa2
+    public Loja(String nome,int quantidadeFuncionarios,double salarioBaseFuncionario, Endereco endereco, Data dataFundacao){
+        this.nome=nome;
+        this.quantidadeFuncionarios=quantidadeFuncionarios;
+        this.salarioBaseFuncionario=salarioBaseFuncionario;
+        this.endereco=endereco;
+        this.dataFundacao= dataFundacao;
+    }
+    ////Só nome, qtd funcionarios, end e data - Etapa2
+    public Loja(String nome,int quantidadeFuncionarios,Endereco endereco, Data dataFundacao){
+        this.nome=nome;
+        this.quantidadeFuncionarios=quantidadeFuncionarios;
+        this.salarioBaseFuncionario=-1;   
+        this.endereco=endereco;
+        this.dataFundacao= dataFundacao;
+    }
+    //Métodos de acesso (getters e setters)
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+    public int getQuantidadeFuncionarios(){
+        return this.quantidadeFuncionarios;
+    }
+    public void setQuantidadeFuncionarios(int quantidadeFuncionarios){
+        this.quantidadeFuncionarios=quantidadeFuncionarios;
+    }
+    public double getSalarioBaseFuncionario(){
+        return this.salarioBaseFuncionario;
+    }
+    public void setSalarioBaseFuncionario(double salarioBaseFuncionario){
+        this.salarioBaseFuncionario=salarioBaseFuncionario;
+    }
+    //Método de acesso - Endereco e dataFundacao
+    public Endereco getEndereco(){
+        return this.endereco;
+    }
+    public void setEndereco(Endereco endereco){
+        this.endereco=endereco;
+    }
+    public Data getDataFundacao(){
+        return this.dataFundacao;
+    }
+    public void setDataFundacao(Data dataFundacao){
+        this.dataFundacao=dataFundacao;
+    }
+    //Método toString()
+
+    public String toString(){
+        return "\nNome da loja: " + this.nome + "\nQuantidade de funcionários: " + this.quantidadeFuncionarios + "\nSalário base dos funcionários: R$" + this.salarioBaseFuncionario + "\nEndereço: " + this.endereco.toString() + "\nData de fundação: " + this.dataFundacao.toString();
+    }
+    //Método gastos com salário
+    public double gastosComSalario(){
+        if (this.salarioBaseFuncionario!=-1)
+            return quantidadeFuncionarios*salarioBaseFuncionario;
+        else
+            return -1;
+    }
+    //Metodo tamanho da loja
+    public char tamanhoDaLoja(){
+        if (this.quantidadeFuncionarios>=0 && quantidadeFuncionarios<10)
+            return 'P';
+        else if (this.quantidadeFuncionarios>=10 && quantidadeFuncionarios<=30)
+            return 'M';
+        else if (this.quantidadeFuncionarios>=31)
+            return 'G';
+        else
+            return ' ';
+    }
+    //Teste toString
+    public static void main(String[] args){
+        Endereco endereco = new Endereco("Rua dos Andradas","Porto Alegre","RS","Brasil","90000-100","1234","Andar Térreo");
+        Data data = new Data(16,5,2018);
+        Loja loja = new Loja("Paquetá",9,2189.90, endereco,data);
+        System.out.println(loja.toString());
+    }
+}
---
+++ Desafio_Etapa2/readme.md
@@ -0,0 +1,15 @@
+---
+Olá!
+
+Esta é a segunda etapa do desafio da disciplina Laboratório I do curso de Análise e Desenvolvimento de Sistemas (Unisinos 2024/01).
+O desafio consiste em criar um gerenciador de lojas para um shopping center em Java.<br>
+
+A segunda etapa contempla a associação entre as classes já criadas, bem como a adaptação e criação de métodos para essa nova realidade.
+Também foi criada a classe Principal com o método main, com um menu para a criação de lojas e de produtos.
+---
+
+Título do Projeto: Desafio Lab01 - Etapa 02<br>
+Objetivo do Projeto: Criar associações entre as classes do gerenciador de lojas para um shopping center e criar menu para cadastro de lojas e produtos.<br>
+Versão: v02<br>
+Data: 22/05/2024<br>
+Autora: Adrianne Ayumi Izu
